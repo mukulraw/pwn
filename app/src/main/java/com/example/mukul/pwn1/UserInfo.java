c4d61dd4af527e3958d3a6e5e8c5118c8ea0543f
mukul <mukulraw199517@gmail.com>
2016-12-30 10:50:20 -0800

    asd

--- app/src/main/java/com/example/mukul/pwn1/UserInfo.java
+++ app/src/main/java/com/example/mukul/pwn1/UserInfo.java
@@ -1,7 +1,10 @@
 package com.example.mukul.pwn1;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 public class UserInfo extends AppCompatActivity {
@@ -14,6 +17,19 @@ public class UserInfo extends AppCompatActivity {
         setContentView(R.layout.activity_user_info);
 
         submit = (TextView)findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Dialog dialog = new Dialog(UserInfo.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setCancelable(true);
+                dialog.setContentView(R.layout.success_dialog);
+                dialog.show();
+
+            }
+        });
 
     }
 }
